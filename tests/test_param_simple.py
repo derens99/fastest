6c754f83abb97727473f2d254f37cdb97428a1c2
Deren Singh <derens99@gmail.com>
2025-05-26 22:32:33 -0400

    fix: tests

--- tests/test_param_simple.py
+++ tests/test_param_simple.py
@@ -1,20 +1,21 @@
 """Simple parametrized test file without pytest dependency."""
 
+import pytest
 
 # We'll simulate parametrize with decorators that fastest can parse
-# @pytest.mark.parametrize("x,y,expected", [(2, 3, 5), (4, 5, 9), (10, -5, 5)])
+@pytest.mark.parametrize("x,y,expected", [(2, 3, 5), (4, 5, 9), (10, -5, 5)])
 def test_addition(x, y, expected):
     """Test addition with multiple inputs."""
     assert x + y == expected
 
 
-# @pytest.mark.parametrize("word", ["hello", "world", "fastest"])
+@pytest.mark.parametrize("word", ["hello", "world", "fastest"])
 def test_length(word):
     """Test string length."""
     assert len(word) > 0
 
 
-# @pytest.mark.parametrize("input,expected", [("hello", "HELLO"), ("world", "WORLD")])
+@pytest.mark.parametrize("input,expected", [("hello", "HELLO"), ("world", "WORLD")])
 def test_upper(input, expected):
     """Test string upper method."""
     assert input.upper() == expected
